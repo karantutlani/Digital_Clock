db77c17702b14393052fb22fe2afd46520beee67
karantutlani <karantutlani@gmail.com>
2025-08-02 22:25:34 -0400

    removed background

--- digitalClock.py
+++ digitalClock.py
@@ -5,7 +5,7 @@ window.title("Digital Clock")
 window.geometry("600x300")
 window.configure(bg="steelblue")
 
-label = Label(window, text="welcome", font=("Arial Black",78,"bold"), bg="steelblue", fg="white" )
+label = Label(window, font=("Arial Black",78,"bold"), bg="steelblue", fg="white" )
 label.pack(pady= 100)
 
 
